07e05f379147b81bcf0838444cda9190153fde68
Zichun (Jerry) Gao <zichungao88@gmail.com>
2023-05-19 22:01:47 -0400

    Fix response issues

--- bot.py
+++ bot.py
@@ -2,10 +2,14 @@ import discord
 import reaction
 
 
-async def send_msg(msg, user_msg, private):
+async def message(msg, user_msg, private):
     try:
         response = reaction.respond(user_msg)
-        await msg.author.send(response) if private else await msg.channel.send(response)
+        if private:
+            await msg.author.send(response)
+        else:
+            await msg.channel.send(response)
+
     except Exception as e:
         print(e)
 
@@ -16,21 +20,21 @@ def run():
     client = discord.Client(intents=intents)
 
     @client.event
-    async def bot_ready():
+    async def on_ready():
         print('LeGM back at it')
 
     @client.event
-    async def bot_msg(msg):
+    async def on_message(msg):
         if msg.author == client.user:
             return
-        username = str(msg.author)
+
         user_msg = str(msg.content)
-        channel = str(msg.channel)
-        if user_msg[0] == '!':
+
+        if user_msg[0] == '-':
             user_msg = user_msg[1:]
-            await send_msg(msg, user_msg, private=True)
+            await message(msg, user_msg, private=True)
         else:
-            await send_msg(msg, user_msg, private=False)
+            await message(msg, user_msg, private=False)
 
     with open('token.txt', 'r') as file:
         TOKEN = file.read()
--- reaction.py
+++ reaction.py
@@ -3,11 +3,21 @@ import random
 
 def respond(msg: str) -> str:
     message = msg.lower()
-    greetings = ['wassup', 'wsg', 'sheesh']
+
+    greetings = ['wsg', 'what up', 'yo', 'hyd', 'sheesh']
     trolls = ['bruh', 'boi', 'stfu', 'no u']
+    ratios = ['L', 'L + ratio', 'job + ratio', 'counter ratio', 'stfu']
 
-    if message == 'hi' or message == 'hello':
+    # sample commands/responses
+    if message == 'hello' or message == 'hi':
         return random.choice(greetings)
-
-    if message == 'ur mom':
+    elif message == 'ur mom' or message == 'L':
         return random.choice(trolls)
+    elif message == 'ratio':
+        return random.choice(ratios)
+    elif message == '!flight':
+        return 'https://tenor.com/view/yummy-tongue-flight-flight-reacts-excited-gif-17057509'
+    elif message == '!flight2':
+        return 'https://tenor.com/view/flightreacts-cringe-gif-19831193'
+    elif message == '!help':
+        return '`List of all commands (tbd)`'
